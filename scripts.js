b76b148627790a5fda4e59da8d6db6e80796f83a
Renee <156127959+reneexie01@users.noreply.github.com>
2024-04-08 06:47:45 +1000

    Created basic add, subtract, multiply, and divide functions

---
+++ scripts.js
@@ -0,0 +1,24 @@
+function sumValues(a, b) {
+    let result = a + b
+    return result;
+}
+
+function subtractValues(a, b) {
+    let result = a - b;
+    return result;
+}
+
+function multiplyValues(a, b) {
+    let result = a * b;
+    return result;
+}
+
+function divideValues(a, b) {
+    let result = a / b;
+    return result;
+}
+
+console.log(sumValues(10, 5));
+console.log(subtractValues(10, 5));
+console.log(multiplyValues(10, 5));
+console.log(divideValues(10, 5));
