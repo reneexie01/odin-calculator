e1c02fde305b2c02f53e49fd2fa5c361a96e3524
Renee <156127959+reneexie01@users.noreply.github.com>
2024-04-12 06:34:44 +1000

    Updated operate function to reduce repetition and fixed operandBtn to operate upon receiving value2

--- scripts.js
+++ scripts.js
@@ -11,6 +11,7 @@ const display = document.querySelector("#display");
 let value1 = 0;
 let operator = "";
 let value2 = 0;
+let result = 0;
 
 operatorBtns.forEach((button) => {
     button.addEventListener("click", () => {
@@ -38,13 +39,23 @@ operandBtns.forEach((button) => {
             display.innerText = input1;
             value1 = Number(input1);
             console.log("value1: ", value1);
-        } else if (!value1 == 0) {
+        } else if (value1 !== 0 && value2 == 0) {
             let input2 = "";
             input2 += button.textContent;
             display.innerText = input2;
             value2 = Number(input2);
             console.log("value2: ", value2);
-        }
+            operate(value1, operator, value2);
+        } /* else if (value2 !== 0) {
+            value1 = result;
+            let input2 = "";
+            input2 += button.textContent;
+            display.innerText = input2;
+            value2 = Number(input2);
+            console.log("value1: ", value1)
+            console.log("value2: ", value2);
+            operate(value1, operator, value2);
+        } */
     })
 })
 
@@ -62,39 +73,36 @@ equalsBtn.addEventListener("click", () => operate(value1, operator, value2));
 
 function sum(a, b) {
     let result = a + b
-    console.log("result: ", result);
-    display.innerText = result;
+    return result;
 }
 
 function subtract(a, b) {
     let result = a - b;
-    console.log("result: ", result);
-    display.innerText = result;
+    return result;
 }
 
 function multiply(a, b) {
     let result = a * b;
-    console.log("result: ", result);
-    display.innerText = result;
+    return result;
 }
 
 function divide(a, b) {
     let result = a / b;
-    console.log("result: ", result);
-    display.innerText = result;
+    return result;
 }
 
 function operate(value1, operator, value2) {
     if (operator === "+") {
-        return sum(value1, value2);
+        result = sum(value1, value2);
     } else if (operator === "-") {
-        return subtract(value1, value2);
+        result = subtract(value1, value2);
     } else if (operator === "*") {
-        return multiply(value1, value2);
+        result = multiply(value1, value2);
     } else if (operator === "/") {
-        divide(value1, value2);
+        result = divide(value1, value2);
     } else {
-        return "Invalid operator input";
+        return console.log("Invalid operator input");
     }
+    console.log("result operate function: ", result);
+    display.innerText = result;
 }
-
