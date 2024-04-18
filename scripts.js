b1c320e0051aad75818da9eb7dde9a8523c8d08c
Renee <156127959+reneexie01@users.noreply.github.com>
2024-04-18 16:15:43 +1000

    Fixed AC bug

--- scripts.js
+++ scripts.js
@@ -12,7 +12,9 @@ let input1 = "";
 let input2 = "";
 
 window.addEventListener("keydown", (event) => {
+
     if (event.key === "." && display.innerText.includes(".")) {
+        console.log("v1: ", value1, "operator: ", operator, "v2: ", value2, "result: ", result)
         return;
     } else {
         if (event.key >= 0 || event.key <= 9 || event.key === ".") {
@@ -21,14 +23,17 @@ window.addEventListener("keydown", (event) => {
                 input1 += event.key;
                 display.innerText = input1;
                 value1 = Number(input1);
+                console.log("v1: ", value1, "operator: ", operator, "v2: ", value2, "result: ", result)
             } else if (operator === "") {
                 input1 += event.key;
                 display.innerText = input1;
                 value1 = Number(input1);
+                console.log("v1: ", value1, "operator: ", operator, "v2: ", value2, "result: ", result)
             } else if (operator !== "" && result === "") {
                 input2 += event.key;
                 display.innerText = input2;
                 value2 = Number(input2);
+                console.log("v1: ", value1, "operator: ", operator, "v2: ", value2, "result: ", result)
             } else if (result !== "") {
                 input2 = "";
                 value2 = "";
@@ -37,33 +42,43 @@ window.addEventListener("keydown", (event) => {
                 value1 = result;
                 value2 = Number(input2);
                 result = "";
+                console.log("v1: ", value1, "operator: ", operator, "v2: ", value2, "result: ", result)
             }
         }
     }
 });
+
 
 operatorBtns.forEach((button) => {
     button.addEventListener("click", () => {
         if (operator === "") {
             if (button.textContent === "+") {
                 operator = "+";
+                console.log("v1: ", value1, "operator: ", operator, "v2: ", value2, "result: ", result)
             } else if (button.textContent === "-") {
                 operator = "-";
+                console.log("v1: ", value1, "operator: ", operator, "v2: ", value2, "result: ", result)
             } else if (button.textContent === "x") {
                 operator = "*";
+                console.log("v1: ", value1, "operator: ", operator, "v2: ", value2, "result: ", result)
             } else if (button.textContent === "/") {
                 operator = "/";
+                console.log("v1: ", value1, "operator: ", operator, "v2: ", value2, "result: ", result)
             }
         } else {
             operate(value1, operator, value2);
             if (button.textContent === "+") {
                 operator = "+";
+                console.log("v1: ", value1, "operator: ", operator, "v2: ", value2, "result: ", result)
             } else if (button.textContent === "-") {
                 operator = "-";
+                console.log("v1: ", value1, "operator: ", operator, "v2: ", value2, "result: ", result)
             } else if (button.textContent === "x") {
                 operator = "*";
+                console.log("v1: ", value1, "operator: ", operator, "v2: ", value2, "result: ", result)
             } else if (button.textContent === "/") {
                 operator = "/";
+                console.log("v1: ", value1, "operator: ", operator, "v2: ", value2, "result: ", result)
             }
         }
     })
@@ -79,14 +94,17 @@ appendBtns.forEach((button) => {
                 input1 += button.textContent;
                 display.innerText = input1;
                 value1 = Number(input1);
+                console.log("v1: ", value1, "operator: ", operator, "v2: ", value2, "result: ", result)
             } else if (operator === "") {
                 input1 += button.textContent;
                 display.innerText = input1;
                 value1 = Number(input1);
+                console.log("v1: ", value1, "operator: ", operator, "v2: ", value2, "result: ", result)
             } else if (operator !== "" && result === "") {
                 input2 += button.textContent;
                 display.innerText = input2;
                 value2 = Number(input2);
+                console.log("v1: ", value1, "operator: ", operator, "v2: ", value2, "result: ", result)
             } else if (result !== "") {
                 input2 = "";
                 value2 = "";
@@ -95,6 +113,7 @@ appendBtns.forEach((button) => {
                 value1 = result;
                 value2 = Number(input2);
                 result = "";
+                console.log("v1: ", value1, "operator: ", operator, "v2: ", value2, "result: ", result)
             }
         }
     })
@@ -105,22 +124,23 @@ clearBtn.addEventListener("click", () => clear());
 function clear() {
     value1 = 0;
     operator = "";
-    value2 = 0;
-    result = 0;
+    value2 = "";
+    result = "";
     input1 = "";
     input2 = "";
     display.innerText = 0;
+    console.log("v1: ", value1, "operator: ", operator, "v2: ", value2, "result: ", result)
 }
 
 equalsBtn.addEventListener("click", () => {
     if (value1 !== 0 && operator !== "" && value2 !== "") {
         operate(value1, operator, value2);
-        value1 = 0;
+        value1 = result;
         operator = "";
         value2 = 0;
         input1 = "";
         input2 = "";
-    input2 = "";
+        console.log("v1: ", value1, "operator: ", operator, "v2: ", value2, "result: ", result)
     } else {
         clear();
     }
